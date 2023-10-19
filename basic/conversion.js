d5e02b467c2d8e24279a0f72e225464f0afc7445
vikaskushwaha <vikas.vikas.kushwaha@gmail.com>
2023-10-19 20:22:09 +0000

    learn datatype and variable

---
+++ basic/conversion.js
@@ -0,0 +1,16 @@
+// let score = "33abc";
+// console.log(typeof (score));
+// let valueInNumber=Number(score);
+// console.log(typeof (valueInNumber));
+
+// "33"=>33
+// "33abc"=>NaN
+
+let isLoggedIn = "";
+let booleanIsloggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsloggedIn)
+
+
+let  sonNum=33;
+let stingNUm = String(sonNum);
+console.log(typeof stingNUm);
