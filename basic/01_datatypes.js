37d5aa5231099e5ea003defc01e77d73819d3e9a
vikaskushwaha <vikas.vikas.kushwaha@gmail.com>
2023-10-19 20:08:42 +0000

    container config

---
+++ basic/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"// treat all js code as newer version
+
+// code readbility 
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean 
+// numll =>standalone value
+// undifined =>
+// symbols
+// objct
+
+console.log(typeof null);
---
+++ basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553;
+let accoutnemail= "vikas.vikas.kushwaha@gmail.com";
+var accoutpass = "1234";
+accountCity="delhi";
+
+accoutnemail = "vika@gmail.com"
+accountPassword = "21212121";
+accountcity = "bengaluru";
+
+console.table([accountId,accoutnemail,accoutpass,accountCity])
